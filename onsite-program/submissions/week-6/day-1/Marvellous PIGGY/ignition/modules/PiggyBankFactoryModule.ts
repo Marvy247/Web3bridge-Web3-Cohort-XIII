ff6c517e27511ba1d37b3afb0419669ad09bd29a
Marvy247 <marvellousdvd@gmail.com>
2025-08-11 14:25:47 +0100

    Completed the  PiggyBank Factory System with ERC20/ETH Support

---
+++ onsite-program/submissions/week-6/day-1/Marvellous PIGGY/ignition/modules/PiggyBankFactoryModule.ts
@@ -0,0 +1,7 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+export default buildModule("PiggyBankFactoryModule", (m) => {
+  const piggyBankFactory = m.contract("PiggyBankFactory");
+
+  return { piggyBankFactory };
+});
